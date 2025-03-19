ccee12ca6a5342afc463413d40a0464980795455
chebishev <atanas.chebishev@gmail.com>
2025-03-19 15:29:36 +0200

    edit the path for manage.py

--- build.sh
+++ build.sh
@@ -5,9 +5,6 @@ set -o errexit
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r requirements.txt
 
+cd what_to_eat
 # Convert static asset files
 python manage.py collectstatic --no-input
-
-# Apply any outstanding database migrations
-cd what_to_eat
-python manage.py migrate
