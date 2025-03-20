9aa1b010941b8a0141a355296d9eb53ddc9db59f
chebishev <atanas.chebishev@gmail.com>
2025-03-20 14:41:04 +0200

    make password reset link

--- what_to_eat/what_to_eat/settings.py
+++ what_to_eat/what_to_eat/settings.py
@@ -120,3 +120,5 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
+
+EMAIL_BACKEND='django.core.mail.backends.console.EmailBackend'
