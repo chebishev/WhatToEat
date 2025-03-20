88ac02a625608a90db4ed1b6cb6171e8f55efc87
chebishev <atanas.chebishev@gmail.com>
2025-03-20 09:25:59 +0200

    fix example env add basic login form to test view functionallity add logout view

--- what_to_eat/users/views.py
+++ what_to_eat/users/views.py
@@ -1,7 +1,27 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
 from .forms import LoginForm
 
 
 def login(request):
-    form = LoginForm()
-    return render(request, 'users/login.html', {'form': form})
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+                login(request, user)
+                return HTTPResponse("User logged in")
+            else:
+                return HttpResponse("Invalid login")
+    else:
+        form = LoginForm()
+        return render(request, 'users/login.html', {'form': form})
+
+def logout(request):
+    request.user.logout()
+    return HttpResponse("User logged out")
