a6d1494fae4f9c8017b0c3923d79615c3282e637
chebishev <atanas.chebishev@gmail.com>
2025-03-20 08:25:48 +0200

    Add STATIC_ROOT

--- what_to_eat/what_to_eat/settings.py
+++ what_to_eat/what_to_eat/settings.py
@@ -113,10 +113,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-print(ALLOWED_HOSTS)
-print(RENDER_EXTERNAL_HOSTNAME)
