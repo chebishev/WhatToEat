5c04089fe834982ed999c9ab21a620a0f584a001
chebishev <atanas.chebishev@gmail.com>
2025-03-20 09:51:30 +0200

    creating logout functionallity

--- what_to_eat/users/urls.py
+++ what_to_eat/users/urls.py
@@ -1,6 +1,9 @@
+from re import template
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('login/', views.login, name='login'),
+    path('login/', views.user_login, name='login'),
+    path('logout/', views.user_logout, name='logout'),
 ]
--- what_to_eat/users/views.py
+++ what_to_eat/users/views.py
@@ -1,27 +1,29 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from .forms import LoginForm
 
 
-def login(request):
+def user_login(request):
     if request.method == 'POST':
         form = LoginForm(request.POST)
 
         if form.is_valid():
             username = form.cleaned_data['username']
             password = form.cleaned_data['password']
+            # from django.contrib.auth
             user = authenticate(request, username=username, password=password)
 
             if user is not None:
+                # from django.contrib.auth
                 login(request, user)
-                return HTTPResponse("User logged in")
+                return HttpResponse("User logged in")
             else:
                 return HttpResponse("Invalid login")
     else:
         form = LoginForm()
         return render(request, 'users/login.html', {'form': form})
 
-def logout(request):
-    request.user.logout()
-    return HttpResponse("User logged out")
+def user_logout(request):
+    logout(request)
+    return render(request, 'users/logout.html')
