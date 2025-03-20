0922e0e3308fe7851e4b50e6bf2702cea0f7346e
chebishev <atanas.chebishev@gmail.com>
2025-03-20 16:33:23 +0200

    Debug false

--- what_to_eat/what_to_eat/settings.py
+++ what_to_eat/what_to_eat/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'RENDER' not in os.environ
+DEBUG = False
 
 ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1']
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
