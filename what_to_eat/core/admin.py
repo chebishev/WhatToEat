7ce8fd321afba7b7e3da26dadad73c9b7e7b9743
chebishev <atanas.chebishev@gmail.com>
2025-03-26 14:52:59 +0200

    add meal model

--- what_to_eat/core/admin.py
+++ what_to_eat/core/admin.py
@@ -1,9 +1,22 @@
 from django.contrib import admin
 from import_export import resources
 from import_export.admin import ImportExportActionModelAdmin
+from .models import Meal
 
 # Register your models here.
 # changing admin header
 admin.site.site_header = "What To Eat - Admin Panel"
 # changing admin title
 admin.site.site_title = "What to eat?"
+
+
+class MealResource(resources.ModelResource):
+    class Meta: 
+        model = Meal
+
+
+class MealAdmin(ImportExportActionModelAdmin):
+    resource_class = MealResource
+
+
+admin.site.register(Meal, MealAdmin)
--- what_to_eat/core/models.py
+++ what_to_eat/core/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Meal(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    ingredients = models.TextField()
+    calories = models.IntegerField(null=True, blank=True)
+    
+    is_breakfast = models.BooleanField(default=False)
+    is_lunch = models.BooleanField(default=False)
+    is_dinner = models.BooleanField(default=False)
+
+    usage_count = models.PositiveIntegerField(default=0)  # Tracks how often this meal was suggested
+
+    def __str__(self):
+        return f"{self.name} for {self.user.username}"
+    
+    def increment_usage(self):
+        self.usage_count += 1
+        self.save()
+
